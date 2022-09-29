051f6ad1804ef5c68ad57b360a73bd4e74121c3c
Yugaraj.B <114376104+yugaGit@users.noreply.github.com>
2022-09-29 22:20:12 +0530

    Add files via upload
    in the above program defining the variables in method parameter and don't assign the values in parameter.

---
+++ names.java
@@ -0,0 +1,11 @@
+public class names {
+   void myMethod(String fname) {
+    System.out.println(fname +"Developer");
+  }
+
+  public static void main(String[] args) {
+    myMethod("mani");
+    myMethod("Jeni");
+    myMethod("kani");
+  }
+}
